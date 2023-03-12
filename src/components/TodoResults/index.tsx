c7405d0a4a83850ba56b9cf0b3a3093ddb33a162
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 18:46:34 -0300

    Implement `useAppSelector(selectTodosCompleted)` to get all todos completed

--- src/components/TodoResults/index.tsx
+++ src/components/TodoResults/index.tsx
@@ -1,9 +1,12 @@
+import { useAppSelector } from 'hooks/useStore';
+import { selectTodosCompleted } from 'state/reducers/todos/todoSelectors';
 import './styles.css';
 
 const TodoResults = () => {
   // Fix an ability to calculate completed tasks
+  const todosCompleted = useAppSelector(selectTodosCompleted);
 
-  return <div className="todo-results">Done:</div>;
+  return <div className="todo-results">Done: {todosCompleted}</div>;
 };
 
 export default TodoResults;
