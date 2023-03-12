2ceafee2c7089e23816e74741b33f47f8f5dffc8
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 18:44:07 -0300

    Add `Provider` to implement redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import store from 'state/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
