b4846a4878db7f401edbf209c909151f52b333f6
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 18:35:08 -0300

    Add mocks to test the store

---
+++ src/mock.ts
@@ -0,0 +1,32 @@
+export const dataMock = [
+  {
+    id: 0,
+    label: 'Fix an ability to display all tasks',
+    checked: false,
+  },
+  {
+    id: 1,
+    label: 'Fix a layout, checkboxes should be listed in a column',
+    checked: false,
+  },
+  {
+    id: 2,
+    label: 'Fix an ability to add a new task',
+    checked: false,
+  },
+  {
+    id: 3,
+    label: 'Fix an ability to toggle a task',
+    checked: true,
+  },
+  {
+    id: 4,
+    label: 'Fix an ability to delete a task',
+    checked: false,
+  },
+  {
+    id: 5,
+    label: 'Fix an ability to count completed tasks',
+    checked: true,
+  },
+];
