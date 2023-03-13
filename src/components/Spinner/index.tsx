49ff51cb525023aa2d84abcc0221da1bd832db56
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:15:54 -0300

    Add `Spinner` component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner-circle" />
+    </div>
+  );
+};
+
+export default Spinner;
