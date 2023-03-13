03607bd2327628f84c51ac9be506e66b4c1f88b3
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:21:06 -0300

    Remove `getOne` `updateAll` to improve performance

--- src/api/services/todos.ts
+++ src/api/services/todos.ts
@@ -1,23 +1,11 @@
 import axiosConfig from 'api/axiosConfig';
 import { endpoints } from 'api/endpoints';
-import type { ITodo } from 'state/types/todosTypes';
-import type { TCreateTodo, TUpdateTodo } from '@types';
-
-const getOne = async (id: number): Promise<ITodo> => {
-  const { data } = await axiosConfig.get(endpoints.todos.getOne(id));
-  return data;
-};
-
-const updateTodo = async (id: number, todo: TUpdateTodo): Promise<ITodo> => {
-  const { data } = await axiosConfig.patch(endpoints.todos.getOne(id), todo);
-  return data;
-};
+import type { ITodo, TCreateTodo } from '@types';
 
 // Functions for working with the API
-
 const getTodos = async (): Promise<ITodo[]> => {
   const { data } = await axiosConfig.get(endpoints.todos.getAll);
-  return data;
+  return data.todos;
 };
 
 const createTodo = async (todo: TCreateTodo): Promise<ITodo> => {
@@ -25,10 +13,9 @@ const createTodo = async (todo: TCreateTodo): Promise<ITodo> => {
   return data;
 };
 
-const toggleTodo = async (id: number): Promise<ITodo> => {
-  const todo = await getOne(id);
-  const updatedTodo = { ...todo, checked: !todo.checked };
-  return updateTodo(id, updatedTodo);
+const toggleTodo = async (id: number, checked: boolean): Promise<ITodo> => {
+  const { data } = await axiosConfig.patch(endpoints.todos.toggle(id), { checked });
+  return data;
 };
 
 const deleteTodo = async (id: number): Promise<number> => {
