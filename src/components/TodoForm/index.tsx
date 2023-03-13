305d68606a4e94cba1c18b7b07174df07757109d
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:32:19 -0300

    Add `dispatch` to `Create` `Toggle` `Delete` todo

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,4 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
+import { useAppDispatch } from 'hooks/useStore';
+import { actions } from 'state/reducers/todos/todoActions';
+import { toast } from 'react-toastify';
 
 const TodoForm = () => {
   const newTodoRef = useRef<HTMLInputElement>(null);
@@ -7,6 +10,7 @@ const TodoForm = () => {
     newTodoRef.current?.focus();
   }, []);
 
+  const dispatch = useAppDispatch();
   const [label, setLabel] = useState<string>('');
 
   const handleLabelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,6 +19,17 @@ const TodoForm = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!label) return;
+
+    try {
+      await dispatch(actions.addTodo({ label, checked: false }));
+      toast.success('Task added successfully', { icon: '📝' });
+    } catch (error) {
+      toast.error('Something went wrong');
+    }
+
+    setLabel('');
   };
 
   return (
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,19 +1,56 @@
+import { toast } from 'react-toastify';
+
+import Spinner from 'components/Spinner';
+import TodoListItem from 'components/TodoListItem';
+import { useAppDispatch, useAppSelector } from 'hooks/useStore';
+import { actions } from 'state/reducers/todos/todoActions';
 import './styles.css';
 
 const TodoList = () => {
-  const handleDelete = (todoId: number) => {
-    // Fix an ability to delete task
+  const { todos, loading, error } = useAppSelector(state => state.todos);
+  const dispatch = useAppDispatch();
+
+  const handleDelete = async (todoId: number): Promise<void> => {
+    try {
+      const response = await dispatch(actions.deleteTodo({ id: todoId }));
+
+      if (actions.deleteTodo.rejected.match(response)) throw new Error();
+
+      toast.success('Task was deleted successfully', { icon: '🗑️' });
+    } catch (error) {
+      toast.error('Something went wrong');
+    }
   };
 
-  const toggleCheck = (todoId: number, isChecked: boolean) => {
-    // Fix an ability to toggle task
+  const toggleCheck = async (todoId: number, isChecked: boolean): Promise<void> => {
+    try {
+      const response = await dispatch(actions.toggleTodo({ id: todoId, checked: !isChecked }));
+
+      if (actions.toggleTodo.rejected.match(response)) throw new Error();
+
+      toast.success('Task was updated successfully', { icon: '✅' });
+    } catch (error) {
+      toast.error('Something went wrong');
+    }
   };
 
   return (
     <div className="todo-list">
       <span className="todo-list-title">Things to do:</span>
-      <div className="todo-list-content">{/* Fix an ability to render todos */}</div>
-      <div className="no-todos">Looks like you&apos;re absolutely free today!</div>
+      <div className="todo-list-content">
+        {todos?.map(todo => (
+          <TodoListItem
+            key={todo.id}
+            onCheck={() => toggleCheck(todo.id, todo.checked)}
+            onDelete={() => handleDelete(todo.id)}
+            {...todo}
+          />
+        ))}
+        {loading === 'loading' && <Spinner />}
+        {error && <span className="todo-list-error">{error}</span>}
+      </div>
+
+      {todos.length <= 0 && <div className="no-todos">Looks like you&apos;re absolutely free today!</div>}
     </div>
   );
 };
