ec8f7dc18cd163d595aeaf6762d93c018b935477
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:21:54 -0300

    Add `TodoForm` `ToastContainer` components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import TodoList from 'components/TodoList';
 import TodoResults from 'components/TodoResults';
+import TodoForm from 'components/TodoForm';
+
+import { useAppDispatch } from 'hooks/useStore';
+import { actions } from 'state/reducers/todos/todoActions';
+
 import './App.css';
 
 const App: FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(actions.getTodos());
+  }, [dispatch]);
+
   return (
     <div className="root">
       <TodoList />
       <TodoResults />
+      <TodoForm />
 
-      <ToastContainer />
+      <ToastContainer pauseOnFocusLoss={false} />
     </div>
   );
 };
