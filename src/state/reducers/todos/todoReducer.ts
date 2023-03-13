d3bf7df1eadf0d2dfd4c56ceb00ab02da5d242db
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:30:32 -0300

    Change `extraReducers` to implement `builder` object

--- src/state/reducers/todos/todoReducer.ts
+++ src/state/reducers/todos/todoReducer.ts
@@ -3,13 +3,11 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { actions } from './todoActions';
 import NAME_REDUCER from 'state/constants';
 
-import type { ITodo, ITodoState } from 'state/types/todosTypes';
-
-// TODO: Remove this mock data
-import { dataMock } from 'mock';
+import { TFulfilledAction, TPendingAction, TRejectedAction } from 'state/types/storeTypes';
+import type { ITodo, ITodoState } from '@types';
 
 const initialState: ITodoState = {
-  todos: dataMock,
+  todos: [],
   loading: 'idle',
   error: null,
 };
@@ -18,64 +16,61 @@ const todoSlice = createSlice({
   name: NAME_REDUCER.todos,
   initialState,
   reducers: {},
-  extraReducers: {
-    // Fetching todos
-    [actions.getTodos.pending.type]: state => {
-      state.loading = 'loading';
-    },
-    [actions.getTodos.fulfilled.type]: (state, action: PayloadAction<ITodo[]>) => {
+  extraReducers(builder) {
+    //
+    builder.addCase(actions.getTodos.fulfilled, (state, action: PayloadAction<ITodo[]>) => {
       state.todos = action.payload;
-      state.loading = 'idle';
-    },
-    [actions.getTodos.rejected.type]: state => {
-      state.loading = 'failed';
-      state.error = state.error || 'Failed to fetch todos';
-    },
+    });
 
-    // Adding a todo
-    [actions.addTodo.pending.type]: state => {
-      state.loading = 'loading';
-    },
-    [actions.addTodo.fulfilled.type]: (state, action: PayloadAction<ITodo>) => {
+    builder.addCase(actions.addTodo.fulfilled, (state, action: PayloadAction<ITodo>) => {
       const { label } = action.payload;
       const todoIds = state.todos.map(todo => todo.id);
       const id = Math.max(...todoIds) + 1;
       const newTodo = { id, label, checked: false };
       state.todos = [...state.todos, newTodo];
-      state.loading = 'idle';
-    },
-    [actions.addTodo.rejected.type]: state => {
-      state.loading = 'failed';
-      state.error = state.error || 'Failed to add a todo';
-    },
+    });
 
-    // Toggle a todo
-    [actions.toggleTodo.pending.type]: state => {
-      state.loading = 'loading';
-    },
-    [actions.toggleTodo.fulfilled.type]: (state, action: PayloadAction<ITodo>) => {
-      const { id, checked } = action.payload;
+    builder.addCase(actions.toggleTodo.fulfilled, (state, action: PayloadAction<ITodo>) => {
+      const { id } = action.payload;
       const todo = state.todos.find(todo => todo.id === id);
-      if (todo) todo.checked = checked;
-      state.loading = 'idle';
-    },
-    [actions.toggleTodo.rejected.type]: state => {
-      state.loading = 'failed';
-      state.error = state.error || 'Failed to toggle a todo';
-    },
+      if (todo) {
+        todo.checked = !todo.checked;
+      }
+    });
 
-    // Deleting a todo
-    [actions.deleteTodo.pending.type]: state => {
-      state.loading = 'loading';
-    },
-    [actions.deleteTodo.fulfilled.type]: (state, action: PayloadAction<number>) => {
-      state.todos = state.todos.filter(todo => todo.id !== action.payload);
-      state.loading = 'idle';
-    },
-    [actions.deleteTodo.rejected.type]: state => {
-      state.loading = 'failed';
-      state.error = state.error || 'Failed to delete a todo';
-    },
+    builder.addCase(actions.deleteTodo.fulfilled, (state, action: PayloadAction<number>) => {
+      const { payload: id } = action;
+      state.todos = state.todos.filter(todo => todo.id !== id);
+    });
+
+    // Match any action that ends in '/pending'
+    builder.addMatcher<TPendingAction>(
+      (action): action is TPendingAction => action.type.endsWith('/pending'),
+      state => {
+        state.loading = 'loading';
+        state.error = null;
+      },
+    );
+
+    // Match any action that ends in '/fulfilled'
+    builder.addMatcher<TFulfilledAction>(
+      (action): action is TFulfilledAction => action.type.endsWith('/fulfilled'),
+      state => {
+        state.loading = 'idle';
+        state.error = null;
+      },
+    );
+
+    // Match any action that ends in '/rejected'
+    builder.addMatcher<TRejectedAction>(
+      (action): action is TRejectedAction => action.type.endsWith('/rejected'),
+      state => {
+        state.loading = 'failed';
+        state.error = state.error || 'Something went wrong. The API is read-only.';
+      },
+    );
+
+    builder.addDefaultCase(state => state);
   },
 });
 
