30b40c0470e29aaf0ceaaf3a6a0bf20c550bfece
Matías Castañeda Mondaca <matiascmondaca@gmail.com>
2023-03-12 22:28:14 -0300

    Add `GenericType` to returned response

--- src/state/types/storeTypes.ts
+++ src/state/types/storeTypes.ts
@@ -1,6 +1,12 @@
-import { ThunkAction, Action } from '@reduxjs/toolkit';
+import { ThunkAction, Action, AsyncThunk } from '@reduxjs/toolkit';
 import store from 'state/store';
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+
+export type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>;
+
+export type TFulfilledAction = ReturnType<GenericAsyncThunk['fulfilled']>;
+export type TPendingAction = ReturnType<GenericAsyncThunk['pending']>;
+export type TRejectedAction = ReturnType<GenericAsyncThunk['rejected']>;
